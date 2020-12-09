7329ac4b258b93e944180579d2b416953bb79600
SarahCORALIE <saraco90@gmail.com>
2020-12-09 19:41:29 +0100

    e

---
+++ eval-js-jquery-SarahCoralie.js
@@ -0,0 +1,25 @@
+// Sarah CORALIE
+//-----------------------------------
+$('#carouselExampleSlidesOnly').carousel({
+    interval: 15000,
+    pause: false
+});
+
+$(document).ready(function(){
+
+// menu déroulant
+//-------------------------------------
+$('.fa-bars').on('click', function()
+{
+    $('.menuToggle').slideToggle(1000)
+});
+
+//scroll infini
+//-------------------------------------
+/*   var listeArticles = $('div.article');
+    console.log($('div.article'))
+
+    
+
+*/
+});
