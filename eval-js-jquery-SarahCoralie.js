83b64bb0dd956172796079016571544abe8f4bbd
SarahCORALIE <saraco90@gmail.com>
2021-01-08 21:52:49 +0100

    carrousel ok

--- eval-js-jquery-SarahCoralie.js
+++ eval-js-jquery-SarahCoralie.js
@@ -6,59 +6,40 @@ $('#carouselExampleSlidesOnly').carousel({
 });
 
 // RENDRE ACTIVES LES FLECHES PREVIOUS ET NEXT DU 1ER CAROUSEL ARTICLE
-$('.carousel-control-prev-icon').on('click', function(){
-    console.log( 'test clic sur previous');
-    $('#carouselExampleControls-1').carousel('prev');
-});
+    // $('.carousel-control-prev-icon').on('click', function(){
+    //     console.log( 'test clic sur previous');
+    //     $('#carouselExampleControls-1').carousel('prev');
+    // });
 
-$('.carousel-control-next-icon').on('click', function(){
-    console.log( 'test clic sur next');
-    $('#carouselExampleControls-1').carousel('next');
-});
+    // $('.carousel-control-next-icon').on('click', function(){
+    //     console.log( 'test clic sur next');
+    //     $('#carouselExampleControls-1').carousel('next');
+    // });
 
 // RENDRE ACTIVES LES FLECHES PREVIOUS ET NEXT POUR TOUS LES CAROUSELS ARTICLE
 
-//let carouselArticle = $('.carousel_article')
-//console.log ('test carouselArticle',carouselArticle)
+    let carouselArticle = $('.carousel_article')
+    console.log ('test carouselArticle',carouselArticle)
 
 //--------------------
-// essaie 1
+//essai 3
 //--------------------
+let previous = carouselArticle.children('.carousel-control-prev');
 
-// $.each(carouselArticle, function(item){
+for ( i of previous ){
+   console.log('test i dans previous',i);
+   previous.on('click', function(){
+       $(this).parent().carousel('prev');
+   })  
+}
 
-//     $('item .carousel-control-prev-icon').on('click', function(){
-//             console.log( 'test clic sur previous');
-//             $(item).carousel('prev');
-//             console.log (item);
-//         });
-        
-//         $('.carousel-control-next-icon').on('click', function(){
-//             console.log( 'test clic sur next');
-//             $(this.parent).carousel('next');
-
-//         }); 
-// }) 
-
-//-------------------
-// essaie 2
-//-------------------
-
-// $('.carousel-control-prev-icon').on('click', function(){
-//     console.log( 'test clic sur previous');
-//     $.each(carouselArticle, function(item)
-//     {     
-//        // $(item).carousel('prev');
-//         console.log ('test item' ,item);
-//         console.log ('test this', this);
-    
-//     }); 
-// }); 
-// $('.carousel-control-next-icon').on('click', function(){
-//     console.log( 'test clic sur next');
-//     $(this.parent).carousel('next');
-// });
-
+let next = carouselArticle.children('.carousel-control-next');
+for ( i of next ){
+   console.log('test i dans previous',i);
+    next.on('click', function(){
+       $(this).parent().carousel('next');
+    })
+}
 
 $(document).ready(function(){
 
